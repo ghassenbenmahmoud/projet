573c3537ea29ee3fcb3dada78744c6d75d9be4e5
ghassenbenmahmoud <94763719+ghassenbenmahmoud@users.noreply.github.com>
2022-02-20 22:57:16 +0100

    Update Test.java

--- src/main/java/main/Test.java
+++ src/main/java/main/Test.java
@@ -5,12 +5,12 @@ public class Test {
 		HabitationIndividuelle h1=new HabitationIndividuelle("houda","ariana",1502,5,true);
 		HabitationIndividuelle h2=new HabitationIndividuelle("ghassen","ariana",1201,8,false);
 		HabitationProfessionnelle P1=new HabitationProfessionnelle("ahmed","ariana",840,40);
-		HabitationProfessionnelle P2=new HabitationProfessionnelle("yessine","ariana",1470,80);
+		
         Commune C=new Commune("Habitation");
         C.ajoutHabitation(h1);
         C.ajoutHabitation(h2);
         C.ajoutHabitation(P1);
-        C.ajoutHabitation(P2);
+       
         C.affiche();
         System.out.println(C.impotCommune());
 }
